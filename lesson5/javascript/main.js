916a7abd65a805ecb0a9a8b41edfb4adaa4505a7
pwatson25 <peterwatson122@gmail.com>
2021-02-06 15:19:15 +0100

    master

--- lesson5/javascript/main.js
+++ lesson5/javascript/main.js
@@ -7,8 +7,23 @@ for (let i = 0; i < navlinks.length; i++) {
     }
 }
 
-let today = new Date();
+function insertannoucement() {
+    let today = new Date();
+    console.log(today.getDay())
+    if(today.getDay() == 5){
+        document.getElementById('annoucementtext').innerHTML = 
+        '<h3>Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.</h3>';
+    } else {
+    const bannershow = document.getElementById('annoucementtext');
+    {bannershow.classList.add('notfriday')}
+    }
+}
 
-if(today.getDay() == 5){
-    document.getElementById('day-text').innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
-};
+function myFunction() {
+    var x = document.getElementById("myNavBar");
+      if (x.className === "navbar") {
+        x.className += " responsive";
+      } else {
+        x.className = "navbar";
+      }
+  }
