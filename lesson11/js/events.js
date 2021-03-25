f29454cd5d529c12dcc8297a6a95a53588738e78
pwatson25 <peterwatson122@gmail.com>
2021-03-25 14:11:20 +0100

    master

--- lesson11/js/events.js
+++ lesson11/js/events.js
@@ -1,5 +1,3 @@
-// Town Events JSON Reference
-
 const eventsURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(eventsURL)
@@ -9,19 +7,16 @@ fetch(eventsURL)
     .then(function (jsonObject) {
 
         const eventsobj = jsonObject['towns'];
-        // If Else logic to detect main_h2 Id Element for injecting town information to the correct page
-        if (document.getElementById("main_h2").innerHTML.indexOf("Preston") != -1) {
-            var eventtown = eventsobj.filter(index => index.name.includes('Preston'));
-        } else if (document.getElementById("main_h2").innerHTML.indexOf("Soda") != -1) {
-            var eventtown = eventsobj.filter(x => x.name.includes('Soda'));
+        if (document.getElementById("town-tag").innerHTML.indexOf("Preston") != -1) {
+            var townevent = eventsobj.filter(index => index.name.includes('Preston'));
+        } else if (document.getElementById("town-tag").innerHTML.indexOf("Soda") != -1) {
+            var townevent = eventsobj.filter(x => x.name.includes('Soda'));
         } else {
-            var eventtown = eventsobj.filter(x => x.name.includes('Fish'));
+            var townevent = eventsobj.filter(x => x.name.includes('Fish'));
         }
 
-        // console.log(eventtown);
-
-        for (i = 0; i < eventtown[0].events.length; i++) {
-            document.getElementById("events").innerHTML += "<p>" + eventtown[0].events[i] + "</p>";
+        for (i = 0; i < townevent[0].events.length; i++) {
+            document.getElementById("events").innerHTML += "<p>" + townevent[0].events[i] + "</p>";
         }
 
     });
