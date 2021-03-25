755a6de7f143b53348159f547ff53c3105d5714a
pwatson25 <peterwatson122@gmail.com>
2021-03-25 10:10:10 +0100

    master

--- lesson11/js/lastUpdate.js
+++
@@ -1,5 +0,0 @@
-const lastModDate = new Date(document.lastModified);
-let strLMD = + lastModDate.getMonth()+1 + "/" + lastModDate.getDate() + "/" 
-           + lastModDate.getFullYear() + " " + lastModDate.getHours() + ":" 
-           + lastModDate.getMinutes() + ":" + lastModDate.getSeconds();
-document.getElementById("lastUpdate").textContent = strLMD;
